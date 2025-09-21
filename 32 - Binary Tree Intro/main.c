0692c451a27943cd350f47f36fccbdbdc32d9c84
Krish-tech-007 <krish.sharma2111@gmail.com>
2025-09-21 19:00:45 +0530

    Binary Tree Intro

---
+++ 32 - Binary Tree Intro/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Binary Tree is a tree in which every node's degree is 2 or less than 2
+// Thus a tree in which every node has at most 2 children is known as a binary tree
+
+// Binary tree is of degree <= 2
+// Each node can have 0/1/2 children
+int main()
+{
+    return 0;
+}
