f1ca26d799af12eec685847319486c99234f12b2
Krish-tech-007 <krish.sharma2111@gmail.com>
2025-09-22 20:08:02 +0530

    Traversal intro

--- 35 - Traversal in Binary Tree Intro/main.c
+++ 35 - Traversal in Binary Tree Intro/main.c
@@ -1,8 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+/*
+Traversal means visiting every node of a data structure at least once
+Traversal in linear data structures can be done primarily in two ways:
+1. Front to rear
+2. Rear to front
+
+Traversal in Binary Tree:
+1. PreOrder  - Root Left Right
+2. InOrder   - Left Root Right
+3. PostOrder - Left Right Root
+*/
+
+/*
+For example, we have a tree:
+
+        7
+       / \
+      2   3
+     /     \
+    1       4
+
+Pre order   : 7 2 1 3 4
+In order    : 1 2 7 3 4
+Post order  : 1 2 4 3 7
+
+Keep asking the questions in order when we visit each node as per the given order of traversal
+
+*/
 int main()
 {
-    printf("Hello world!\n");
+
     return 0;
 }
