fc6c3a97f2a7af52f3aa94c655b4a0682b5f7c48
Krish-tech-007 <krish.sharma2111@gmail.com>
2025-09-25 10:57:08 +0530

    Binary Search Tree

---
+++ 39 - Binary Search Tree Into/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/*
+Binary Search Tree:
+1. It is a type of binary tree
+
+Properties:
+1. All nodes of the left subtree are lesser
+2. All nodes of the right subtree are greater
+3. Left and Right Subtrees are also binary search trees (Ensured by first two properties)
+4. There are no duplicate nodes (Ensured by first two properties)
+5. In order traversal of a Binary Search Tree gives an ascending sorted array (Can be used to determine if a tree is BST or not)
+To check if the array is sorted or not, check if current index element is smaller than next index element or not
+*/
+
+/*
+Some benefits of Binary Search Tree:
+1. Search is efficient
+2. We can keys such that we can search it in logn time
+3. We can insert data such that when we search for it in the future, we do so in minimum time
+*/
+
+/*
+Example of Binary Search Tree:
+            9
+          /   \
+         4     11
+        / \      \
+       2   7      13
+          / \       \
+         5   8       14
+*/
+int main()
+{
+
+    return 0;
+}
---
+++ 40 - Checking if tree is BST or not/main.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node{
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+
+void init(struct Node **node, int data){
+    *node = (struct Node *) malloc(sizeof(struct Node));
+    (*node)->data = data;
+    (*node)->left = NULL;
+    (*node)->right = NULL;
+}
+
+void inOrderTraversal(struct Node *root,int *arr, int *index){
+    if(root!=NULL){
+        inOrderTraversal(root->left, arr, index);
+        arr[(*index)++] = root->data;
+        inOrderTraversal(root->right, arr, index);
+    }
+}
+
+int isSorted(int *arr, int size){
+    for(int i = 0; i<size-1; i++){
+        if(*(arr+i) > *(arr+i+1))
+            return 0;
+    }
+    return 1;
+}
+
+/*
+Example of Binary Search Tree:
+            9
+          /   \
+         4     11
+        / \      \
+       2   7      13
+          / \       \
+         5   8       14
+*/
+int main()
+{
+    struct Node *root, *c1, *c2, *gc1, *gc2, *gc3, *ggc1, *ggc2, *ggc3;
+    init(&root, 9);
+    init(&c1, 4);
+    init(&c2, 11);
+    init(&gc1, 2);
+    init(&gc2, 7);
+    init(&gc3, 13);
+    init(&ggc1, 5);
+    init(&ggc2, 8);
+    init(&ggc3, 14);
+
+    root->left = c1;
+    root->right = c2;
+
+    c1->left = gc1;
+    c1->right = gc2;
+
+    c2->right = gc3;
+
+    gc2->left = ggc1;
+    gc2->right = ggc2;
+
+    gc3->right = ggc3;
+
+
+    int *inOrder = (int *) malloc(sizeof(int) * 9);
+    int index = 0;
+    printf("Going into function\n");
+    inOrderTraversal(root,inOrder, &index);
+
+    if(isSorted(inOrder, 9))
+        printf("Given Tree is a Binary Search Tree\n");
+    else
+        printf("Given Tree is not a Binary Search Tree\n");
+
+    return 0;
+}
